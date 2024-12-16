c1da99412bb5e4441c8c547a0a75c205f9439a9d
Anubhav Singh <86763919+Anubhav8176@users.noreply.github.com>
2024-12-16 20:45:44 +0530

    Configuration

--- src/main/java/com/anucodes/authservice/config/SecurityConfig.java
+++ src/main/java/com/anucodes/authservice/config/SecurityConfig.java
@@ -1,32 +1,79 @@
 package com.anucodes.authservice.config;
 
 
+import com.anucodes.authservice.repository.UserRepository;
+import com.anucodes.authservice.services.UserDetailsServiceImpl;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@Data
 public class SecurityConfig {
 
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private final UserDetailsServiceImpl userDetailsService;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    @Autowired
+    public UserDetailsService userDetailsService(UserRepository userRepository, PasswordEncoder passwordEncoder){
+        return new UserDetailsServiceImpl(userRepository, passwordEncoder);
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, JwtAuthFilter jwtAuthFilter) throws Exception {
 
         return httpSecurity
                 .csrf(CsrfConfigurer::disable)
                 .cors(CorsConfigurer::disable)
                 .authorizeHttpRequests(auth->
                         auth
-                                .requestMatchers("/auth/login","/auth/signup").permitAll()
+                                .requestMatchers("/auth/login","/auth/signup","/auth/refreshToken").permitAll()
                                 .anyRequest().authenticated()
                 )
+                .sessionManagement(sess->
+                        sess
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .authenticationProvider(authenticationProvider())
                 .build();
 
+    }
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
     }
     
 }
